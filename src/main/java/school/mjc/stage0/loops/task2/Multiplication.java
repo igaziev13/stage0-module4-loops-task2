350cadd06447c1d027d058cecd22dfdd4c83f29a
IGAZIEV <islam.gaziev@gmail.com>
2022-08-06 15:41:23 +0500

    Module #4.2. Task #2. Attempt #5.

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,17 +1,23 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
-    public void printMultiplied(int multiplyByAndToInclusive) {
+    public static void printMultiplied(int multiplyByAndToInclusive) {
         int i = 0;
         while (i<=multiplyByAndToInclusive)
         {
             System.out.println(i*multiplyByAndToInclusive);
             i++;
         }
+        i=0;
         while (i>=multiplyByAndToInclusive)
         {
             System.out.println(i*multiplyByAndToInclusive);
-            i++;
+            i--;
         }
+    }
+    public static void main(String[] args)
+    {
+        printMultiplied(-5);
+
     }
 }
