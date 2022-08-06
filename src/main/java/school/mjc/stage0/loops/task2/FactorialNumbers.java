83858a13f7f1514ae157c72ea2c79fd858dd392e
IGAZIEV <islam.gaziev@gmail.com>
2022-08-06 15:29:04 +0500

    Module #4.2. Task #2. Attempt #3.

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int i=1;
+        int i=0;
         while (i<=printToInclusive) {
             int j = 1;
             int result = 1;
